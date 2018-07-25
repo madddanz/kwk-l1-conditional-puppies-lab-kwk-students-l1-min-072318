35413ddfa2eb32e60f8ff4b9edd16f01e2e1b2d6
madddanz <maddiedanzberger@gmail.com>
2018-07-25 14:06:46 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -3,7 +3,7 @@ def no_puppies (puppies)
   if puppies > 0 
     "No More Puppies!"
   elsif puppies == 0 
-    "Well done, Rachel!"
+    "Good Job, Rachel!"
   end
 end
 
@@ -29,10 +29,6 @@ less_puppies(5)
 
 
 # 3. We've decided to give Rachel a little more control over her puppy obsession. Write a method called some_puppies that takes in two arguments - the number of puppies she has found and a maximum puppy allowance. This method should tell Rachel good job if she gets less than half her maximum allowance and to get back on her puppy restriction when she exceeds her maximum.
-
-def some_puppies(pupppies)
-  
-
 
 
 # BONUS
